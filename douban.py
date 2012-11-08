b269327ad8a9e9064870557bffe0f65e900b19f4
leiqin <leiqin2010@gmail.com>
2012-11-09 01:37:15 +0800

    可以播放 douban 音乐，还未处理 cookie

--- douban.py
+++ douban.py
@@ -1,32 +1,73 @@
 #!/usr/bin/python
 # encoding=utf-8
 
-import glob
-
-g = glob.iglob("/home/leiqin/Music/*.mp3")
+import urllib
+import urllib2
+import json
             
 class Douban(object):
 
     cookiefile = "~/.cache/python-doubanfm/cookie.txt"
 
+    # http://douban.fm/j/mine/playlist?type=e&sid=221320&channel=0&pt=213.4&from=mainsite&r=a2d009faac
+    url = 'http://douban.fm/j/mine/playlist'
+
     def __init__(self):
-        pass
+        self.opener = urllib2.build_opener()
+        self.songs = []
+
+    def _open(self, type='n', sid=None, channel=0, pt=None):
+        params = {}
+        if type:
+            params['type'] = type
+        if sid:
+            params['sid'] = sid
+        if channel != None:
+            params['channel'] = channel
+        if pt != None:
+            params['pt'] = '%.1f' % pt
+        url = self.url
+        if params:
+            url = ''.join([url, '?', urllib.urlencode(params)])
+        return self.opener.open(url)
+
+    def _parse(self, response):
+        j = json.load(response)
+        self.songs = j['song']
 
     def next(self, song = None):
-        if song:
-            print song.time
+        if not song:
+            # new
+            if not self.songs:
+                res = self._open()
+                self._parse(res)
+        elif song.time >= song.length:
+            # reach end
+            res = self._open(type='e', sid=song.sid, pt=song.time)
+            res.close()
+            if not self.songs:
+                res = self._open(type='p', sid=song.sid, pt=0)
+                self._parse(res)
         else:
-            song = Song()
-        song.url = g.next()
-        print song.url
-        return song
+            # hand
+            res = self._open(type='s', sid=song.sid, pt=song.time)
+            self._parse(res)
+
+        json = self.songs.pop()
+        result = Song(json)
+        if not self.songs:
+            res = self._open(type='p', sid=result.sid, pt=0)
+            self._parse(res)
+        return result
 
 
     def like(self, song):
-        pass
+        res = self._open(type='r', sid=result.sid, pt=song.time)
+        self._parse(res)
 
     def unlike(self, song):
-        pass
+        res = self._open(type='u', sid=result.sid, pt=song.time)
+        self._parse(res)
         
 class Song(object):
 
@@ -42,4 +83,11 @@ class Song(object):
         self.picture = self.data.get('picture')
         self.length = self.data.get('length')
         self.time = 0
+        self.file = None
+
+
+if __name__ == '__main__':
+    douban = Douban()
+    s = douban.next()
+    print s.title, s.sid, s.url
 
