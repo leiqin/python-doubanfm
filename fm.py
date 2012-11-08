055be70853029cd18a76cde8bc0dc47bc29743bd
leiqin <leiqin2010@gmail.com>
2012-11-08 16:16:10 +0800

    处理命令行参数

--- fm.py
+++ fm.py
@@ -1,2 +1,42 @@
 #!/usr/bin/python
 # encoding=utf-8
+
+import argparse
+import os
+import os.path
+
+import listeningfifo
+import douban
+
+parser = argparse.ArgumentParser(description="CmdLine for http://douban.fm . you can use cookie file export from firefox , put it to %s" % douban.Douban.cookiefile)
+
+parser.add_argument('-s', '--server', action='store_true', help="Start the server")
+
+parser.add_argument('-p', '--play', action='store_const', const='p', dest="flag", help="Start play")
+parser.add_argument('-P', '--pause', action='store_const', const='P', dest="flag", help="Pause")
+parser.add_argument('-G', '--toggle-pause', action='store_const', const='G', dest="flag", help="Toggle between play/pause")
+
+parser.add_argument('-n', '--next', action='store_const',const='n',dest="flag", help="Play next song")
+
+parser.add_argument('-l', '--like', action='store_const', const='l', dest="flag", help="Like current song")
+parser.add_argument('-u', '--unlike', action='store_const', const='u', dest="flag", help="Unlike current song")
+parser.add_argument('-L', '--toggle-like', action='store_const', const='L', dest="flag", help="Toggle between like/unlike")
+
+parser.add_argument('-x', '--exit', action='store_true', help="Shutdown the server")
+
+args = parser.parse_args()
+
+def writepipe(ch):
+    pipefile = listeningfifo.pipefile
+    with open(pipefile, 'w') as p:
+        p.write(ch)
+
+if args.exit:
+    writepipe('x')
+elif args.server:
+    listeningfifo.start()
+elif args.flag:
+    writepipe(args.flag)
+else:
+    parser.print_help()
+
--- listeningfifo.py
+++ listeningfifo.py
@@ -1,67 +1,63 @@
 #!/usr/bin/python
 # encoding=utf-8
 
-import threading
 import os
 import os.path
 
 import player
 import util
 
-class Listening(threading.Thread):
 
-    pipefile = "~/.cache/pythen-doubanfm/pipe"
+pipefile = os.path.expanduser("~/.cache/pythen-doubanfm/pipe")
 
-    def run(self):
-        p = player.Player()
-        p.play()
-        p.start()
+def start():
+    p = player.Player()
+    p.play()
+    p.start()
 
-        pipefile = os.path.expanduser(self.pipefile)
-        self.mkpipe(pipefile)
+    mkpipe(pipefile)
 
-        f = open(pipefile, 'r')
-        while True:
-            cmd = f.read(1)
-            if not cmd:
-                f.close()
-                f = open(pipefile, 'r')
-            elif cmd == 'n':
-                p.next()
-            elif cmd == 's':
-                if not p.playing:
-                    p.play()
-            elif cmd == 'p':
-                if p.playing:
-                    p.pause()
-            elif cmd == 'P':
-                if p.playing:
-                    p.pause()
-                else:
-                    p.play()
-            elif cmd == 'l':
-                if not p.song.like:
-                    p.like()
-            elif cmd == 'u':
-                if p.song.like:
-                    p.unlike()
-            elif cmd == 'L':
-                if p.song.like:
-                    p.unlike()
-                else:
-                    p.like()
-            elif cmd == 'x':
-                p.exit()
-                os.remove(pipefile)
-                return
-
-    def mkpipe(self, pipefile):
-        util.initParent(pipefile)
-        if os.path.exists(pipefile):
+    f = open(pipefile, 'r')
+    while True:
+        cmd = f.read(1)
+        if not cmd:
+            f.close()
+            f = open(pipefile, 'r')
+        elif cmd == 'n':
+            p.next()
+        elif cmd == 'p':
+            if not p.playing:
+                p.play()
+        elif cmd == 'P':
+            if p.playing:
+                p.pause()
+        elif cmd == 'G':
+            if p.playing:
+                p.pause()
+            else:
+                p.play()
+        elif cmd == 'l':
+            if not p.song.like:
+                p.like()
+        elif cmd == 'u':
+            if p.song.like:
+                p.unlike()
+        elif cmd == 'L':
+            if p.song.like:
+                p.unlike()
+            else:
+                p.like()
+        elif cmd == 'x':
+            p.exit()
             os.remove(pipefile)
-        os.mkfifo(pipefile, 0600)
+            return
+
+def mkpipe(pipefile):
+    util.initParent(pipefile)
+    if os.path.exists(pipefile):
+        os.remove(pipefile)
+    os.mkfifo(pipefile, 0600)
         
 
 if __name__ == "__main__":
-    l = Listening()
-    l.run()
+    start()
